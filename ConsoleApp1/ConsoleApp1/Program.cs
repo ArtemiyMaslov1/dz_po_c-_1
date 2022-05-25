5c16fe34d5ad3981aecb0b6c664481b601731a5b
Артем Маслов <90208966+ArtemiyMaslov1@users.noreply.github.com>
2022-05-25 18:45:47 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WriteLine(0b11);
+            //WriteLine(0b1011);
+            //WriteLine(0xA1); 
+            //WriteLine(2.2e3);
+            //WriteLine('\x78'); // айсики код
+            //WriteLine('\u0428'); // юни код
+            //object a = 22;
+            //object b = 3.14;
+            //object c = "hello code";
+            //WriteLine(a);
+            //WriteLine(b);
+            //WriteLine(c);
+            //var hello = "hello world";
+            //WriteLine(hello);
+
+            //string name1 = "Tom";
+            //int age = 24;
+            //double height = 1.88;
+
+            //WriteLine("Имя: {0} Возраст: {2} Рост: {1}", name1, height, age);
+            //WriteLine("Введите свое имя: ");
+            //string name = ReadLine();
+            //WriteLine($"Привет {name}");
+
+            //int? nullint = null;
+            //nullint = nullint ?? 50; // ?? проверка на пустоту
+
+            //BackgroundColor = ConsoleColor.Red;
+            //ForegroundColor = ConsoleColor.Green;
+            //WriteLine("Is CAPS LOCK turned  " + "on: " + CapsLock.ToString());
+            //SetBufferSize(100, 100);
+            //SetWindowSize(100, 100);
+
+
+
+
+            //WriteLine("Задание 1: ");
+            //WriteLine("Введите сторону А: ");
+            //int A = Convert.ToInt32(ReadLine());
+            //WriteLine("Введите сторону Б: ");
+            //int B = Convert.ToInt32(ReadLine());
+            //WriteLine("Введите сторону С: ");
+            //int C = Convert.ToInt32(ReadLine());
+
+            //if(A*B > C*C)
+            //{
+            //    WriteLine($"Кол-во квадратов в прямоугольнике = {(A * B) / (C * C)}, осталось места {(A * B) % (C * C)}" );
+            //}
+            //if(A*B < C*C)
+            //{
+            //    WriteLine("Ошибка!!! В прямоугольник не помещается квадрат!!!");
+            //}
+
+
+
+            //WriteLine("Задание 3: ");
+            //WriteLine("Введите число А: ");
+            //int A = Convert.ToInt32(ReadLine());
+            //WriteLine("Введите число В: ");
+            //int B = Convert.ToInt32(ReadLine());
+
+            //for(; A <= B; A++)
+            //{
+            //    for(int i = 0; i < A; i++)
+            //    {
+            //        Write(($"{A}"));
+
+            //    }
+            //    WriteLine("");
+            //}
+
+            //WriteLine("Задание 2: ");
+            //WriteLine("Введите процент по вкладу: ");
+            //float P = Convert.ToInt32(ReadLine());
+            //float vklad = 10000;
+            //int i;
+            //for (i = 0; vklad < 11000;i++)
+            //{
+            //    vklad = vklad * (P / 100 + 1);
+     
+            //}
+            //WriteLine($"Кол-во месяцев: {i}, итоговый размер вклада: {vklad}");
+        }
+
+    }
+}
